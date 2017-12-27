a9e4d4796685554ad330de4a2cf5aabad442b070
Nitzanel <nitzan@claysciences.com>
2017-12-27 19:30:14 +0200

    Fixed some style issues in compare_exons.py

--- compare_utils/compare_exons.py
+++ compare_utils/compare_exons.py
@@ -1,26 +1,32 @@
-import sys
-sys.path.append('..')
 from dochap_tools.common_utils import utils
 import sqlite3 as lite
 import re
+import sys
+sys.path.append('..')
 
 
 expression = re.compile(r'(?<=\[)([0-9:]*)(?=\])')
 
-def get_exons_from_transcript_id(root_dir,specie,transcript_id):
+
+def get_exons_from_transcript_id(root_dir, specie, transcript_id):
+    """
+    Query the database and return list of dictionaries expressing exons data
+    """
     # query the knownGene table
-    with lite.connect(utils.get_specie_db_path(root_dir,specie)) as conn:
+    with lite.connect(utils.get_specie_db_path(root_dir, specie)) as conn:
         conn.row_factory = lite.Row
-        known_gene_transcript = get_known_gene_transcript(conn,transcript_id)
+        known_gene_transcript = get_known_gene_transcript(conn, transcript_id)
         exons = get_exons_from_transcript_dict(known_gene_transcript)
     return exons
 
-def get_known_gene_transcript(conn,transcript_id):
+
+def get_known_gene_transcript(conn, transcript_id):
     cursor = conn.cursor()
     query = f'SELECT * from knownGene WHERE name = ?'
-    cursor.execute(query,(transcript_id,))
+    cursor.execute(query, (transcript_id, ))
     result = cursor.fetchone()
     return result
+
 
 def get_exons_from_transcript_dict(transcript_data):
     exons = []
@@ -31,12 +37,12 @@ def get_exons_from_transcript_dict(transcript_data):
         start = int(starts[index])
         end = int(ends[index])
         length = abs(start-end)
-        exons.append({'length':length,'real_start':start,'real_end':end})
+        exons.append({'length':length, 'real_start':start, 'real_end':end})
     set_relative_exons_position(exons)
     return exons
 
 
-def set_relative_exons_position(exons,start_mod=0):
+def set_relative_exons_position(exons, start_mod=0):
     last_end = 0
     for exon in exons:
         exon['relative_start'] = last_end + start_mod + 1
@@ -45,39 +51,39 @@ def set_relative_exons_position(exons,start_mod=0):
     return exons
 
 
-def get_domains_of_gene(root_dir,specie,gene_name):
+def get_domains_of_gene(root_dir, specie, gene_name):
     """
     reuturn list of lists of domains dictionaries, for every variant of the gene.
     """
-    path = utils.get_specie_db_path(root_dir,specie)
+    path = utils.get_specie_db_path(root_dir, specie)
     with lite.connect(path) as conn:
         conn.row_factory = lite.Row
         cursor = conn.cursor()
-        query = "SELECT sites,regions from genbank WHERE symbol = ?"
-        cursor.execute(query,(gene_name,))
+        query = "SELECT sites, regions from genbank WHERE symbol = ?"
+        cursor.execute(query, (gene_name, ))
         results = cursor.fetchall()
         domains_variants = []
         for gene_result in results:
-            domains = combine_sites_and_regions(gene_result['sites'],gene_result['regions'])
+            domains = combine_sites_and_regions(gene_result['sites'], gene_result['regions'])
             domains_variants.append(domains)
     return domains_variants
 
 
-def combine_sites_and_regions(sites_string,regions_string):
+def combine_sites_and_regions(sites_string, regions_string):
     """
     reuturn list of domains dictionaries
     """
-    sites = extract_domains_data(sites_string,'site')
-    regions = extract_domains_data(regions_string,'region')
+    sites = extract_domains_data(sites_string, 'site')
+    regions = extract_domains_data(regions_string, 'region')
     domains = sites+regions
     return domains
 
 
-def extract_domains_data(domains_string,dom_type):
-    domain_strings_list = re.findall(expression,domains_string)
+def extract_domains_data(domains_string, dom_type):
+    domain_strings_list = re.findall(expression, domains_string)
     domains_description = domains_string.split(r'],')
     domains = []
-    for index,domain_string in enumerate(domain_strings_list):
+    for index, domain_string in enumerate(domain_strings_list):
         if ':' in domain_string:
             split = domain_string.split(':')
             if len(split) != 2:
@@ -86,66 +92,47 @@ def extract_domains_data(domains_string,dom_type):
             start = (int(split[0])+1) * 3 - 2
             end = (int(split[1])+1) * 3
             description = domains_description[index]+']'
-            domains.append({'type':dom_type,'start' : start,'end':end,'description':description})
+            domains.append({'type':dom_type, 'start':start, 'end':end, 'description':description})
     return domains
 
 
-def get_transcript_id_of_gene(conn,gene_name):
+def get_transcript_id_of_gene(conn, gene_name):
     """
     Return transcript_id of given gene name
     """
     cursor = conn.cursor()
     query = 'SELECT * from alias WHERE alias = ?'
-    cursor.execute(query,(gene_name,))
+    cursor.execute(query, (gene_name, ))
     result = cursor.fetchone()
     return result['transcript_id']
 
-def get_gene_aliases_of_transcript_id(conn,transcript_id):
+
+def get_gene_aliases_of_transcript_id(conn, transcript_id):
     """
     return all known aliases of a given transcript id
     """
     cursor = conn.cursor()
     query = 'SELECT * from alias WHERE transcript_id = ?'
-    cursor.execute(query,(transcript_id,))
-    result = cursor.fetchall()
+    cursor.execute(query, (transcript_id, ))
+    results = cursor.fetchall()
     aliases = []
     for result in results:
         aliases.append(result['gene_alias'])
     return aliases
 
-def compare_intersections(intersection,candidates):
-    """
-    sort a list of intersection candidates in comparison to given intersection
-    """
-    for candidate in candidates:
-        score = get_intersections_score(intersection,candidate)
-        candidate['score'] = score
-    candidates.sort(key=lambda i:i['score'])
-    return candidates
 
-def get_intersections_score(i1,i2):
+def get_domains_intersections_in_exons(domains_list, exons_list):
     """
-    compare two intersection and return the score
-    """
-    if i1['domain_index'] == i2['domain_index']:
-        i1_length = abs(i1['start'] - i1['end'])
-        i2_length = abs(i2['start'] - i2['end'])
-        score = i1_length / i2_length
-        if score == 0:
-            return 0.0
-        if score > 1.0:
-            return 1.0/score
-        else:
-            return score
-    else:
-        return 0.0
+    return all the intersections between a list of domains and a list of exons.
+    the intersections dictionary contains keys of exon's index (str) and value of list of intersections
 
-def get_domains_intersections_in_exons(domains_list,exons_list):
+    {index:intersections[]}
+    """
     intersections = {}
-    for exon_index,exon in enumerate(exons_list):
+    for exon_index, exon in enumerate(exons_list):
         intersections[str(exon_index)] = []
-        for domain_index,domain in enumerate(domains_list):
-            intersection = get_domain_location_in_exon(domain,exon)
+        for domain_index, domain in enumerate(domains_list):
+            intersection = get_domain_intersection_in_exon(domain, exon)
             if intersection:
                 # append the intersection
                 intersection['domain_index'] = domain_index
@@ -157,8 +144,11 @@ def get_domains_intersections_in_exons(domains_list,exons_list):
     return intersections
 
 
-def get_domain_location_in_exon(domain,exon):
-    intersection = {'start':None,'end':None}
+def get_domain_intersection_in_exon(domain, exon):
+    """
+    return intesection between domain and exon, or None if no intersection exists.
+    """
+    intersection = {'start':None, 'end':None}
     e_start = exon['relative_start']
     e_end = exon['relative_end']
     d_start = domain['start']
@@ -176,7 +166,30 @@ def get_domain_location_in_exon(domain,exon):
     return None
 
 
+def compare_intersections(intersection, candidates):
+    """
+    sort a list of intersection candidates in comparison to given intersection
+    """
+    for candidate in candidates:
+        score = get_intersections_score(intersection, candidate)
+        candidate['score'] = score
+    candidates.sort(key=lambda i:i['score'])
+    return candidates
 
 
-
-
+def get_intersections_score(i1, i2):
+    """
+    compare two intersection and return the score
+    """
+    if i1['domain_index'] == i2['domain_index']:
+        i1_length = abs(i1['start'] - i1['end'])
+        i2_length = abs(i2['start'] - i2['end'])
+        score = i1_length / i2_length
+        if score == 0:
+            return 0.0
+        if score > 1.0:
+            return 1.0/score
+        else:
+            return score
+    else:
+        return 0.0
